9b1209dde545a2cad76f0de6b3e842b53a08df94
rumborak.de <rumborak.de@1ca96ac1-6748-0410-ba04-a18ab6185271>
2008-12-02 07:44:21 +0000

    fixes

--- trunk/OpenThesaurus_Deutsch/OtherResources/u.js
+++ trunk/OpenThesaurus_Deutsch/OtherResources/u.js
@@ -1,4 +1,4 @@
-var currentVersion = "v2008.12.01";
+var currentVersion = "v2008.12.02";
 
 var req;
 var updateURL = 'http://www.tekl.de/deutsch/OpenThesaurus_Deutsch.html';
--- trunk/OpenThesaurus_Deutsch/preinstall.sh
+++ trunk/OpenThesaurus_Deutsch/preinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 containsUser=`echo $2 | sed 's/^\/Users\/.*$//g'`
-if [$containsUser == ""]; then
+if ["${containsUser}" == ""]; then
     parentID=`ls -nld ~/Library | sed 's/^[^0-9]* [0-9]* \([0-9]*\) .*/\1/g'`
     mkdir ~/Library/Dictionaries
     chown ${parentID} ~/Library/Dictionaries
